8fd91188d616ac74d9a34302ebe16095eb3fe657
yong seock <myongdol132@gmail.com>
2023-08-09 03:01:28 +0900

    리터럴타입

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultType) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,9 +6,16 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultType === '감자') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 }
-var combinedAges = combine(12, 34);
-var combinedNames = combine('LimeFlavor', 'hello');
+var combinedAges = combine(12, 34, '감자');
+var combinedStringAges = combine('12', '34', '감자');
+var combinedNames = combine('LimeFlavor', 'hello', '토마토');
 console.log(combinedAges);
 console.log(combinedNames);
+console.log(combinedStringAges);
--- app.ts
+++ app.ts
@@ -1,14 +1,21 @@
-function combine(input1:number | string, input2:number | string) {
+function combine(input1:number | string, input2:number | string, resultType: string) {
     let result;
     if(typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if(resultType === '감자') {
+         return +result
+    } else {
+        return result.toString();
+    }
 }
 
-const combinedAges = combine(12, 34);
-const combinedNames = combine('LimeFlavor', 'hello');
+const combinedAges = combine(12, 34, '감자');
+const combinedStringAges = combine('12', '34', '감자');
+const combinedNames = combine('LimeFlavor', 'hello', '토마토');
+
 console.log(combinedAges);
 console.log(combinedNames);
+console.log(combinedStringAges);
