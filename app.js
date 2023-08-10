59b956a90a82b1318709524aafb6bd4ee6922bfe
yong seock <myongdol132@gmail.com>
2023-08-10 22:14:56 +0900

    반환 타입과 void

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-function combine(input1, input2, resultType) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === '감자') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if(resultType === '감자') {
-    //      return +result
-    // } else {
-    //     return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(12, 34, '감자');
-var combinedStringAges = combine('12', '34', '감자');
-var combinedNames = combine('LimeFlavor', 'hello', '토마토');
-console.log(combinedAges);
-console.log(combinedNames);
-console.log(combinedStringAges);
+function printResult(num) {
+    console.log('result: ' + num);
+}
+printResult(add(10, 20));
