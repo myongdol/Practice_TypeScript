b93931f5ca19f4fe652b4a77164e7ef0ed06117f
yong seock <myongdol132@gmail.com>
2023-08-12 21:42:22 +0900

    unknown타입 학습

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
 let userInput: unknown;
+let userName: string;
+
+userInput = 1234;
+userInput = 'Myongdol';
+
+if(typeof userInput === 'string') {
+    userName = userInput;
+}
+
+// userName = userInput;
